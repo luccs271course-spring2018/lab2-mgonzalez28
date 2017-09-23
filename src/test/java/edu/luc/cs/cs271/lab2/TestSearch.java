b9d055f1e8681f96d79a304daa2d1a583ce2c31b
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-09-23 23:17:48 +0000

    Updated TestSearch and TestTeam

--- src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
+++ src/test/java/edu/luc/cs/cs271/lab2/TestSearch.java
@@ -2,11 +2,10 @@ package edu.luc.cs.cs271.lab2;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.*;
 import java.util.List;
 
 public class TestSearch {
-  
+
   Team[] makeArrayFixture(final int size) {
     final Team[] array = new Team[size];
     for (int i = 0; i < size; i++) {
@@ -17,15 +16,15 @@ public class TestSearch {
   }
 
   // DONE makeListFixture
-  List<Team> makeListFixture(final int size) {     
-    final List<Team> list = new ArrayList<Team>(); 
+  List<Team> makeListFixture(final int size) {
+    final List<Team> list = new ArrayList<Team>();
     for (int i = 0; i < size; i++) {
       final String s = Integer.toString(i);
       list.add(new Team("Team " + s, "Coach "+ s, i * 100 + 50));
     }
     return list;
-  } 
-    
+  }
+
   @Test
   public void testFindPositionArray0() {
     final Team[] arr = makeArrayFixture(0);
@@ -43,10 +42,26 @@ public class TestSearch {
     final Team[] arr = makeArrayFixture(10);
     assertFalse(Search.findTeamPosition(arr, "Team 11").isPresent());
   }
-  
-  // TODO: testFindPositionList0, 10s, 10f
-  
+
+  // DONE: testFindPositionList0, 10s, 10f
+  @Test
+  public void testFindPositionList0() {
+    final List<Team> list = makeListFixture(0);
+    assertFalse(Search.findTeamPosition(list, "Team 5").isPresent());
+  }
+
+  @Test
+  public void testFindPositionList10s() {
+    final List<Team> list = makeListFixture(10);
+    assertTrue(Search.findTeamPosition(list, "Team 5").isPresent());
+  }
+
+  @Test
+  public void testFindPositionList10f() {
+    final List<Team> list = makeListFixture(10);
+    assertFalse(Search.findTeamPosition(list, "Team 11").isPresent());
+  }
+
   // TODO: testFindMinFundingArray for several sizes and scenarios
 
   // TODO: testFindMinFundingArrayFast for several sizes and scenarios
-}
